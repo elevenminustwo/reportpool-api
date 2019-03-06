226647ed7596e1b9bb6b7ad21e460badca3a1bec
mertio <mert7000@gmail.com>
2019-03-06 21:50:26 +0300

    test commit from Mert

--- src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
+++ src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
@@ -3,7 +3,7 @@ package tr.edu.akdeniz.reportpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Test comment
+// Test comment from Mert
 
 @SpringBootApplication
 public class ReportpoolApplication {
