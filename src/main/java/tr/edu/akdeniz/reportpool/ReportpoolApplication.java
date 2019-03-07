9532273ec7fb7acb3dbb29449002c1137d253ce5
mertio <mert7000@gmail.com>
2019-03-07 21:46:36 +0300

    first push to my branch

--- src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
+++ src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
@@ -3,7 +3,7 @@ package tr.edu.akdeniz.reportpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Test comment from Mert 2
+// Push test to Mert branch
 
 @SpringBootApplication
 public class ReportpoolApplication {
