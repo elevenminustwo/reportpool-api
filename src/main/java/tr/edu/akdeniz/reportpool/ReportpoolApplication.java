96b71d59e47d55c4a100f4c09fe52c60ee9b21ce
huseyineryilmaz <huseyin.eryilmaz1996@gmail.com>
2019-03-18 13:37:09 +0300

    bbbbbb

--- src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
+++ src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
@@ -3,7 +3,7 @@ package tr.edu.akdeniz.reportpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Test comment from huseyın 2
+// Test comment from huusseyın 2
 
 @SpringBootApplication
 public class ReportpoolApplication {
