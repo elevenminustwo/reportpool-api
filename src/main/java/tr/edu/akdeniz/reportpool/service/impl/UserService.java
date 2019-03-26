8d961c1c9da227bec19095e350d33a74ea738a05
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-03-27 00:37:56 +0300

    datatable backend done

--- src/main/java/tr/edu/akdeniz/reportpool/service/impl/UserService.java
+++ src/main/java/tr/edu/akdeniz/reportpool/service/impl/UserService.java
@@ -4,12 +4,12 @@ import org.hibernate.jpa.internal.schemagen.ScriptTargetOutputToFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tr.edu.akdeniz.reportpool.model.PaginationDto;
+import tr.edu.akdeniz.reportpool.model.PersonDto;
 import tr.edu.akdeniz.reportpool.model.UserDto;
 import tr.edu.akdeniz.reportpool.repository.UserRepository;
 import tr.edu.akdeniz.reportpool.service.GenericUserService;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,17 +28,29 @@ public class UserService implements GenericUserService {
                 .map(t -> new UserDto(t.getUserId(),t.getUsername(),t.getEmail(),t.getPassword(),t.getName(),t.getSurname(),t.getIsActive()))
                 .collect(Collectors.toList());
     }
-    public PaginationDto getPersons(){
+    public PaginationDto getPersons(String draw,String length,String skip,String sortDir,String sortColumnIndex,String search){
         Query q = entityManager
                 .createNativeQuery(
-                        "SELECT u.Username,u.Name,u.Surname,u.Email,u.IsActive" +
+                        "SELECT u.Username as username,u.Name as name ,u.Surname,u.Email,u.IsActive" +
                                 " FROM user u" +
                                 " LEFT JOIN userroles u2 on u.UserID = u2.user_id" +
                                 " LEFT JOIN role r on u2.role_id = r.RoleID"+
-                                " LEFT JOIN userunit u3 on u.UserID = u3.user_id").setMaxResults(5);
+                                " LEFT JOIN userunit u3 on u.UserID = u3.user_id" +
+                                " WHERE u.Username LIKE ?1" +
+                                " ORDER BY ?2 ?3")
+                .setParameter(1,"%"+search+"%")
+                .setParameter(2,sortColumnIndex)
+                .setParameter(3,sortDir)
+                .setMaxResults(Integer.parseInt(length))
+                .setFirstResult(Integer.parseInt(skip));
+        int recordsTotal = q.getResultList().size();
+        PaginationDto paginationDto = new PaginationDto();
+        paginationDto.setDraw(Integer.parseInt(draw));
+        paginationDto.setRecordsFiltered(recordsTotal-Integer.parseInt(skip));
+        paginationDto.setRecordsTotal(recordsTotal);
+        paginationDto.setArray(q.getResultList());
 
-        System.out.println(q.getResultList().size());
-        return null;
+        return paginationDto;
     }
 
 }
