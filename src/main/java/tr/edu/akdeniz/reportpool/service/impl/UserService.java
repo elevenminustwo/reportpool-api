1792fed8b24ebc86c82143b5181e4996c2ac1435
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-03-27 04:07:23 +0300

    almost

--- src/main/java/tr/edu/akdeniz/reportpool/service/impl/UserService.java
+++ src/main/java/tr/edu/akdeniz/reportpool/service/impl/UserService.java
@@ -10,6 +10,7 @@ import tr.edu.akdeniz.reportpool.repository.UserRepository;
 import tr.edu.akdeniz.reportpool.service.GenericUserService;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,11 +32,19 @@ public class UserService implements GenericUserService {
     public PaginationDto getPersons(String draw,String length,String skip,String sortDir,String sortColumnIndex,String search){
         Query q = entityManager
                 .createNativeQuery(
-                        "SELECT u.Username as username,u.Name as name ,u.Surname,u.Email,u.IsActive" +
+                        "SELECT u.UserID as userId," +
+                                "u.Username as username," +
+                                "u.Name as name ," +
+                                "u.Surname as surname," +
+                                "u.Email as email," +
+                                "u.IsActive as isActive," +
+                                "r.RoleName as rolename," +
+                                "u4.Name as unit" +
                                 " FROM user u" +
                                 " LEFT JOIN userroles u2 on u.UserID = u2.user_id" +
                                 " LEFT JOIN role r on u2.role_id = r.RoleID"+
                                 " LEFT JOIN userunit u3 on u.UserID = u3.user_id" +
+                                " LEFT JOIN unit u4 on u3.unit_id = u4.UnitID" +
                                 " WHERE u.Username LIKE ?1" +
                                 " ORDER BY ?2 ?3")
                 .setParameter(1,"%"+search+"%")
@@ -48,7 +57,12 @@ public class UserService implements GenericUserService {
         paginationDto.setDraw(Integer.parseInt(draw));
         paginationDto.setRecordsFiltered(recordsTotal-Integer.parseInt(skip));
         paginationDto.setRecordsTotal(recordsTotal);
-        paginationDto.setArray(q.getResultList());
+        List<Object[]> list = q.getResultList();
+        List<PersonDto> personDtos = new ArrayList<>();
+        for (Object[] a : list) {
+            personDtos.add(new PersonDto(a[0].toString(),a[1].toString(),a[2].toString(),a[3].toString(),a[4].toString(),a[5].toString(),"",""));
+        }
+        paginationDto.setArray(personDtos);
 
         return paginationDto;
     }
