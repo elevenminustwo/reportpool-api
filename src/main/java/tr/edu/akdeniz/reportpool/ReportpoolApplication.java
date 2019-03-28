fa9b11eee9d12e097e8c8885055ef5482ed4b15a
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-03-28 18:10:23 +0300

    merged

--- src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
+++ src/main/java/tr/edu/akdeniz/reportpool/ReportpoolApplication.java
@@ -3,10 +3,14 @@ package tr.edu.akdeniz.reportpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tr.edu.akdeniz.reportpool.file_upload.FileStorageProperties;
 
 // Push test to Mert branch
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class ReportpoolApplication {
 
     public static void main(String[] args) {
