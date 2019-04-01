8b71a7d091c6c9abb91cf8909c7d10e25af9e681
Rabia Aktaş <aktasrabiaa@gmail.com>
2019-04-01 20:21:26 +0300

    Unit Reports data table

--- src/main/java/tr/edu/akdeniz/reportpool/controller/UnitReportController.java
+++ src/main/java/tr/edu/akdeniz/reportpool/controller/UnitReportController.java
@@ -1,10 +1,15 @@
 package tr.edu.akdeniz.reportpool.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tr.edu.akdeniz.reportpool.model.PaginationDto;
 import tr.edu.akdeniz.reportpool.model.UnitReportDto;
 import tr.edu.akdeniz.reportpool.service.impl.UnitReportService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.List;
 
 @RestController
@@ -13,15 +18,31 @@ public class UnitReportController {
     @Autowired
     UnitReportService unitReportService;
 
+
+    @RequestMapping(value = "/unitreports/{dept}/{unit}/")
     @CrossOrigin
-    @RequestMapping(value = "/unitreports/{id}",method = RequestMethod.GET)
-    public List<UnitReportDto> getUnitReports(@PathVariable int id){
-      return  unitReportService.getUnitReports(id);
+    public String getUnitReports(@PathVariable int dept, @PathVariable int unit,HttpServletRequest request) throws JsonProcessingException
+    {
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        Gson gson = new Gson();
+        String search = request.getParameter("search[value]");
+        String sortColumnIndex = request.getParameter("order[0][column]");
+        String sortDir = request.getParameter("order[0][dir]");
+        String skip = request.getParameter("start");
+        String length = request.getParameter("length");
+        String draw = request.getParameter("draw");
+
+        PaginationDto paginationDto = unitReportService.getUnitsReports(dept,unit,draw,length,skip,sortDir,sortColumnIndex,search);
+
+        String jsonString = gson.toJson(paginationDto);
+        return jsonString;
+
     }
 
     @CrossOrigin
     @RequestMapping(value = "tdd/unitreports/{id}",method = RequestMethod.GET)
     public List<UnitReportDto> getUnitReportsTest(@PathVariable int id){
-        return  unitReportService.getUnitReports(id);
+        return  null;
     }
 }
--- src/main/java/tr/edu/akdeniz/reportpool/service/impl/UnitReportService.java
+++ src/main/java/tr/edu/akdeniz/reportpool/service/impl/UnitReportService.java
@@ -1,7 +1,9 @@
 package tr.edu.akdeniz.reportpool.service.impl;
 
+import javafx.scene.control.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tr.edu.akdeniz.reportpool.model.PaginationDto;
 import tr.edu.akdeniz.reportpool.model.UnitReportDto;
 import tr.edu.akdeniz.reportpool.repository.*;
 
@@ -17,13 +19,56 @@ public class UnitReportService {
     EntityManager entityManager;
 
 
-    public List<UnitReportDto> getUnitReports(int i) {
-        Query unitreport = entityManager.createNativeQuery(
-                "SELECT d.Name AS departmenName, u.Name AS unitName , us.Name AS userName , us.Surname , r.DateCompleted , att.Path FROM Department d , Departmentunit du , Unit u , User us , Report r , Attachment att" +
-                        " WHERE d.DepartmentID = du.department_id AND du.unit_id = u.UnitID AND us.UserID = r.user_id AND r.ReportID = att.report_id " +
-                        "AND d.DepartmentID= ?1");
-        unitreport.setParameter(1,i);
 
-        return  unitreport.getResultList();
+    public PaginationDto getUnitsReports(int dept,int unit,String draw,String length,String skip,String sortDir,String sortColumnIndex,String search){
+        switch (Integer.parseInt(sortColumnIndex)){
+            case 0:
+                sortColumnIndex="userName";
+                break;
+            case 1:
+                sortColumnIndex="Surname";
+                break;
+            case 2:
+                sortColumnIndex="DateCompleted";
+                break;
+            case 3:
+                sortColumnIndex="Path";
+                break;
+
+        }
+        Query q=entityManager
+                .createNativeQuery(
+                        "SELECT us.Name  AS ?0," +
+                            " us.Surname AS ?1," +
+                            "r.DateCompleted AS ?2," +
+                             "att.Path AS ?3" +
+                             " FROM department d,departmentunit du,unit u,user us,report r,attachment att,userdepartmentunit udu" +
+                              " WHERE d.DepartmentID=du.department_id AND " +
+                                "u.UnitID=du.unit_id " +
+                                "AND du.DepartmentUnitID= udu.departmentunit_id" +
+                              " AND du.DepartmentUnitID=r.departmentunit_id " +
+                                "AND r.user_id=us.UserID " +
+                               "AND r.ReportID=att.report_id " +
+                               " AND us.Name LIKE ?4"+
+                              " AND d.DepartmentID="+dept+
+                              "  AND u.UnitID= "+unit+
+                               "  ORDER BY "+sortColumnIndex+" "+sortDir.toUpperCase())
+                .setParameter(0,"userName")
+                .setParameter(1,"surName")
+                .setParameter(2,"dateCompleted")
+                .setParameter(3,"Path")
+                .setParameter(4,"%"+search+"%")
+
+                .setMaxResults(Integer.parseInt(length))
+                .setFirstResult(Integer.parseInt(skip));
+                int recordsTotal=((Number)entityManager
+                        .createNativeQuery("SELECT COUNT(*) " +
+                                "FROM department d , departmentunit du , unit u , user us , report r , attachment att" +
+                                " WHERE d.DepartmentID = du.department_id AND du.unit_id = u.UnitID AND us.UserID = r.user_id " +
+                                "AND r.ReportID = att.report_id AND d.DepartmentID= ?7 ")
+                        .setParameter(7,"%"+search+"%")
+                        .getSingleResult()).intValue();
+        PaginationDto paginationDto = new PaginationDto(Integer.parseInt(draw),recordsTotal-Integer.parseInt(skip),recordsTotal,q.getResultList());
+        return paginationDto;
     }
 }
