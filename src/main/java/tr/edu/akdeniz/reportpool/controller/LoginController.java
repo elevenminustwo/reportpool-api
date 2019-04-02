e67d0a01f5b27a15d8c158750588bf9c28410f1e
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-04-03 01:41:53 +0300

    login&register basic done.

---
+++ src/main/java/tr/edu/akdeniz/reportpool/controller/LoginController.java
@@ -0,0 +1,30 @@
+package tr.edu.akdeniz.reportpool.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tr.edu.akdeniz.reportpool.service.impl.UserService;
+
+@RestController
+public class LoginController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/api/login/{username}/{psw}",method = RequestMethod.POST)
+    @CrossOrigin
+    public ResponseEntity login(@PathVariable("username") String username,@PathVariable("psw") String psw) {
+
+        return userService.login(username,psw);
+    }
+    @RequestMapping(value = "/api/register/{email}/{username}/{psw}/{name}/{surname}",method = RequestMethod.POST)
+    @CrossOrigin
+    public ResponseEntity register(
+            @PathVariable("email") String email,
+            @PathVariable("username") String username,
+            @PathVariable("psw") String psw,
+            @PathVariable("name") String name,
+            @PathVariable("surname") String surname) {
+
+        return userService.register(email,username,psw,name,surname);
+    }
+}
